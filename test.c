02702ef0a2a5ad1d8ee691a053dddaabb390ecc4
ksmaybe <ksmaybe@gmail.com>
2019-02-03 11:53:16 -0500

    init

---
+++ test.c
@@ -0,0 +1,16 @@
+//
+// Created by ksmaybe on 2/3/19.
+//
+
+#include <stdio.h>
+
+int main( )
+{
+    int x[4]={1,2,3,4};
+    char c[]="hello";
+    char w[]=" world";
+    printf("%s%s",c,w);
+    printf("%d",x[0]);
+
+    return 0;
+}
